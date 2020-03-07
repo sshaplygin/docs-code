e5fa3765f0928997f23818245a85010284ee2653
Saimon Shaplygin <semyuon@gmail.com>
2020-03-07 18:16:09 +0300

    feat: bik coverage

--- validator_test.go
+++ validator_test.go
@@ -16,8 +16,74 @@ type TestCodeCase struct {
 func TestValidateBIK(t *testing.T) {
 	t.Parallel()
 
-	t.Run("", func(t *testing.T) {
+	t.Run("invalid bik length", func(t *testing.T) {
+		testCases := []TestCodeCase{
+			TestCodeCase{
+				Code:    "1234567888776",
+				Error:   ErrInvalidBIKLength,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "044525",
+				Error:   ErrInvalidBIKLength,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "044525225",
+				Error:   nil,
+				IsValid: true,
+			},
+			TestCodeCase{
+				Code:    "044525012",
+				Error:   nil,
+				IsValid: true,
+			},
+		}
+		for _, test := range testCases {
+			isValid, err := ValidateBIK(test.Code)
+			assert.Equal(t, isValid, test.IsValid, test.Code)
+			assert.Equal(t, true, errors.Is(test.Error, err), test.Code)
+		}
+	})
 
+	t.Run("invalid bik value", func(t *testing.T) {
+		testCases := []TestCodeCase{
+			TestCodeCase{
+				Code:    "0445?5226",
+				Error:   ErrInvalidValue,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "054525225",
+				Error:   ErrInvalidBIKCountryCode,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "104525225",
+				Error:   ErrInvalidBIKCountryCode,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "044#55#25",
+				Error:   ErrInvalidValue,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "044525225",
+				Error:   nil,
+				IsValid: true,
+			},
+			TestCodeCase{
+				Code:    "044525012",
+				Error:   nil,
+				IsValid: true,
+			},
+		}
+		for _, test := range testCases {
+			isValid, err := ValidateBIK(test.Code)
+			assert.Equal(t, isValid, test.IsValid, test.Code, test.IsValid)
+			assert.Equal(t, true, errors.Is(test.Error, err), test.Code)
+		}
 	})
 }
 
@@ -28,47 +94,12 @@ func TestValidateINN(t *testing.T) {
 		testCases := []TestCodeCase{
 			TestCodeCase{
 				Code:    "12345678",
-				Error:   errInvalidINNLength,
+				Error:   ErrInvalidINNLength,
 				IsValid: false,
 			},
 			TestCodeCase{
 				Code:    "9876543211123",
-				Error:   errInvalidINNLength,
-				IsValid: false,
-			},
-			TestCodeCase{
-				Code:    "7707083893",
-				Error:   nil,
-				IsValid: true,
-			},
-			TestCodeCase{
-				Code:    "526317984689",
-				Error:   nil,
-				IsValid: true,
-			},
-		}
-		for _, test := range testCases {
-			isValid, err := ValidateINN(test.INN)
-			assert.Equal(t, isValid, test.IsValid)
-			assert.Equal(t, errors.Is(test.Error, err), true)
-		}
-	})
-
-	t.Run("invalid inn value", func(t *testing.T) {
-		testCases := []TestCodeCase{
-			TestCodeCase{
-				Code:    "77$7083893",
-				Error:   errInvalidValue,
-				IsValid: false,
-			},
-			TestCodeCase{
-				Code:    "98754321N123",
-				Error:   errInvalidValue,
-				IsValid: false,
-			},
-			TestCodeCase{
-				Code:    "9854132d1123",
-				Error:   errInvalidValue,
+				Error:   ErrInvalidINNLength,
 				IsValid: false,
 			},
 			TestCodeCase{
@@ -84,8 +115,43 @@ func TestValidateINN(t *testing.T) {
 		}
 		for _, test := range testCases {
 			isValid, err := ValidateINN(test.Code)
-			assert.Equal(t, isValid, test.IsValid)
-			assert.Equal(t, errors.Is(test.Error, err), true)
+			assert.Equal(t, isValid, test.IsValid, test.Code)
+			assert.Equal(t, true, errors.Is(test.Error, err), test.Code)
+		}
+	})
+
+	t.Run("invalid inn value", func(t *testing.T) {
+		testCases := []TestCodeCase{
+			TestCodeCase{
+				Code:    "77$7083893",
+				Error:   ErrInvalidValue,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "98754321N123",
+				Error:   ErrInvalidValue,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "9854132d1123",
+				Error:   ErrInvalidValue,
+				IsValid: false,
+			},
+			TestCodeCase{
+				Code:    "7707083893",
+				Error:   nil,
+				IsValid: true,
+			},
+			TestCodeCase{
+				Code:    "526317984689",
+				Error:   nil,
+				IsValid: true,
+			},
+		}
+		for _, test := range testCases {
+			isValid, err := ValidateINN(test.Code)
+			assert.Equal(t, isValid, test.IsValid, test.Code)
+			assert.Equal(t, true, errors.Is(test.Error, err), test.Code)
 		}
 	})
 }
