d26a503a77d9c669ed738db3dde11b11fc44afce
Saimon Shaplygin <semyuon@gmail.com>
2020-03-07 16:58:24 +0300

    ref: inn test cases

--- validator_test.go
+++ validator_test.go
@@ -7,6 +7,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type TestCodeCase struct {
+	Code    string
+	IsValid bool
+	Error   error
+}
+
 func TestValidateBIK(t *testing.T) {
 	t.Parallel()
 
@@ -15,34 +21,28 @@ func TestValidateBIK(t *testing.T) {
 	})
 }
 
-type TestINNCase struct {
-	INN     string
-	IsValid bool
-	Error   error
-}
-
 func TestValidateINN(t *testing.T) {
 	t.Parallel()
 
 	t.Run("invalid inn length", func(t *testing.T) {
-		testCases := []TestINNCase{
-			TestINNCase{
-				INN:     "12345678",
+		testCases := []TestCodeCase{
+			TestCodeCase{
+				Code:    "12345678",
 				Error:   errInvalidINNLength,
 				IsValid: false,
 			},
-			TestINNCase{
-				INN:     "9876543211123",
+			TestCodeCase{
+				Code:    "9876543211123",
 				Error:   errInvalidINNLength,
 				IsValid: false,
 			},
-			TestINNCase{
-				INN:     "7707083893",
+			TestCodeCase{
+				Code:    "7707083893",
 				Error:   nil,
 				IsValid: true,
 			},
-			TestINNCase{
-				INN:     "526317984689",
+			TestCodeCase{
+				Code:    "526317984689",
 				Error:   nil,
 				IsValid: true,
 			},
@@ -55,35 +55,35 @@ func TestValidateINN(t *testing.T) {
 	})
 
 	t.Run("invalid inn value", func(t *testing.T) {
-		testCases := []TestINNCase{
-			TestINNCase{
-				INN:     "77$7083893",
+		testCases := []TestCodeCase{
+			TestCodeCase{
+				Code:    "77$7083893",
 				Error:   errInvalidValue,
 				IsValid: false,
 			},
-			TestINNCase{
-				INN:     "98754321N123",
+			TestCodeCase{
+				Code:    "98754321N123",
 				Error:   errInvalidValue,
 				IsValid: false,
 			},
-			TestINNCase{
-				INN:     "9854132d1123",
+			TestCodeCase{
+				Code:    "9854132d1123",
 				Error:   errInvalidValue,
 				IsValid: false,
 			},
-			TestINNCase{
-				INN:     "7707083893",
+			TestCodeCase{
+				Code:    "7707083893",
 				Error:   nil,
 				IsValid: true,
 			},
-			TestINNCase{
-				INN:     "526317984689",
+			TestCodeCase{
+				Code:    "526317984689",
 				Error:   nil,
 				IsValid: true,
 			},
 		}
 		for _, test := range testCases {
-			isValid, err := ValidateINN(test.INN)
+			isValid, err := ValidateINN(test.Code)
 			assert.Equal(t, isValid, test.IsValid)
 			assert.Equal(t, errors.Is(test.Error, err), true)
 		}
