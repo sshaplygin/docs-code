03071a07bb35f2f4477d93155125c12c738615ce
Saimon Shaplygin <semyuon@gmail.com>
2020-03-12 20:35:10 +0300

    feat: implemented all methods

--- validator.go
+++ validator.go
@@ -18,6 +18,8 @@ func strToArr(str string) ([]int, error) {
 	return arr, nil
 }
 
+// IsINNValid
+// example valid format is
 func IsINNValid(inn string) (bool, error) {
 	if len(inn) != 10 && len(inn) != 12 {
 		return false, ErrInvalidINNLength
@@ -35,6 +37,8 @@ func IsINNValid(inn string) (bool, error) {
 	return firstControlNumber == innArr[len(innArr)-2] && secondControlNumber == innArr[len(innArr)-1], nil
 }
 
+// IsBIKValid
+// example valid format is
 func IsBIKValid(bik string) (bool, error) {
 	if len(bik) != 9 {
 		return false, ErrInvalidBIKLength
@@ -55,18 +59,63 @@ func IsBIKValid(bik string) (bool, error) {
 	return code >= 50 && code < 1000, nil
 }
 
+// IsOGRNValid
+// example valid format is
 func IsOGRNValid(ogrn string) (bool, error) {
-	return false, nil
+	if len(ogrn) != 13 {
+		return false, ErrInvalidOGRNLength
+	}
+	ogrnArr, err := strToArr(ogrn)
+	if err != nil {
+		return false, err
+	}
+	code, _ := strconv.Atoi(ogrn[:12])
+	return ogrnArr[len(ogrn)-1] == code%11%10, nil
 }
 
+// IsOGRNIPValid check to valid OGRNIP format
+// example valid format is
 func IsOGRNIPValid(ogrnip string) (bool, error) {
-	return false, nil
+	if len(ogrnip) != 15 {
+		return false, ErrInvalidOGRNIPLength
+	}
+	ogrnipArr, err := strToArr(ogrnip)
+	if err != nil {
+		return false, err
+	}
+	if ogrnipArr[0] != 3 && ogrnipArr[0] != 4 {
+		return false, ErrInvalidValue
+	}
+	code, _ := strconv.Atoi(ogrnip[:12])
+	return ogrnipArr[len(ogrnip)-1] == code%13%10, nil
 }
 
+// IsSNILSValid check
+// example valid format is `112-233-445 95`
 func IsSNILSValid(snils string) (bool, error) {
-	return false, nil
+	if len(snils) != 14 {
+		return false, ErrInvalidSNILSLength
+	}
+	fSnils := strings.ReplaceAll(snils, "-", "")
+	fSnils = strings.ReplaceAll(fSnils, " ", "")
+	if len(snils) != 11 {
+		return false, ErrInvalidFormattedSNILSLength
+	}
+	snilsArr, err := strToArr(fSnils)
+	if err != nil {
+		return false, err
+	}
+	hashSum := 0
+	hashLen := len(snilsArr) - 2
+	code, _ := strconv.Atoi(snils[hashLen:])
+	for i, v := range snilsArr[:hashLen] {
+		hashSum += v * (hashLen - i)
+	}
+	return hashSum%101 == code, nil
 }
 
+// IsKPPValid
+// example valid format is
 func IsKPPValid(kpp string) (bool, error) {
 	if len(kpp) == 9 {
 		return true, nil
