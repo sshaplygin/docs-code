8a9710d87cfc43038a7eabce46ea50487ac579da
Saimon Shaplygin <semyuon@gmail.com>
2020-03-12 20:35:22 +0300

    feat: add new errors types

--- errors.go
+++ errors.go
@@ -3,9 +3,14 @@ package go_codes_validator
 import "errors"
 
 var (
-	ErrInvalidINNLength = errors.New("invalid inn length")
-	ErrInvalidBIKLength = errors.New("invalid bik length")
-	ErrInvalidKPPLength = errors.New("invalid kpp length")
+	ErrInvalidINNLength    = errors.New("invalid inn length")
+	ErrInvalidBIKLength    = errors.New("invalid bik length")
+	ErrInvalidKPPLength    = errors.New("invalid kpp length")
+	ErrInvalidOGRNLength   = errors.New("invalid ogrn length")
+	ErrInvalidOGRNIPLength = errors.New("invalid ogrinp length")
+	ErrInvalidSNILSLength  = errors.New("invalid snils length")
+
+	ErrInvalidFormattedSNILSLength = errors.New("invalid formatted snils length")
 
 	ErrInvalidValue = errors.New("invalid code value")
 
