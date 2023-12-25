ce22c4bc79f8415401a7c6e41c9f21f1cbb88157
Saimon Shaplygin <semyuon@gmail.com>
2023-12-25 08:25:57 +0100

    add: ks package

---
+++ ks/ks_test.go
@@ -0,0 +1,20 @@
+package ks
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Validete(t *testing.T) {
+	require.Panics(t, func() {
+		_, err := Validate("")
+		require.NoError(t, err)
+	})
+}
+
+func Test_Generate(t *testing.T) {
+	require.Panics(t, func() {
+		Generate()
+	})
+}
