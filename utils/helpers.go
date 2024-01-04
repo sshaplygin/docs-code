f7fa0781bfe305fd869cc8cedb68140c9fd376ce
Saimon Shaplygin <semyuon@gmail.com>
2024-01-04 17:12:46 +0100

    add: utils

--- utils/helpers.go
+++ utils/helpers.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"crypto/rand"
+	"fmt"
 	"math/big"
 	"strconv"
 	"strings"
@@ -9,15 +10,32 @@ import (
 	"github.com/sshaplygin/docs-code/models"
 )
 
+// RandomDigits generate random digits required length. Required len > 0.
 func RandomDigits(len int) int64 {
 	if len <= 0 {
 		len = 1
 	}
+
 	max, _ := strconv.Atoi(strings.Repeat("9", len))
 	min, _ := strconv.Atoi("1" + strings.Repeat("0", len-1))
 
 	num, _ := rand.Int(rand.Reader, big.NewInt(int64(max-min+1)))
 	return num.Int64() + int64(min)
+}
+
+// Random generate random digit in range [min, max]. Required max > 0.
+func Random(min, max int) int {
+	if max == 0 || min == max {
+		max += 1
+	}
+
+	randomNumber, err := rand.Int(rand.Reader, big.NewInt(int64(max-min+1)))
+	if err != nil {
+		fmt.Println("Error generating random number:", err)
+		return 0
+	}
+
+	return int(randomNumber.Int64()) + min
 }
 
 func StrToArr(str string) ([]int, error) {
